30e8e4912041943c3a3bdd93c2b439aa4f9d336a
prog-er <olji20020 000@gmail.com>
2024-10-23 23:23:24 +0500

    Initial Commit

---
+++ findFlagApp3/findFlagApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  findFlagApp3App.swift
+//  findFlagApp3
+//
+//  Created by Olzhas Karibayev on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct findFlagApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
